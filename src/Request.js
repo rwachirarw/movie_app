674bfa453add794b93575a14c59615ede968bc30
Edwin Njeru <rwachira.rw@gmail.com>
2023-02-27 19:02:17 +0300

    Added the scroll effect

--- src/Request.js
+++ src/Request.js
@@ -1,7 +1,7 @@
 const key = "8f44aa0aa0b58b425620a10abaaf9e29";
 
 const requests = {
-  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}`,
   requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
   requestTrending: `https://api.themoviedb.org/3/trending/all/day?api_key=${key}`,
   requestHorror: ``,
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { FaHeart, FaRegHeart } from "react-icons/fa";
+
+import Movie from "./Movie";
 
 const Row = ({ title, fetchURL }) => {
   const [movies, setMovies] = useState([]);
-  const [like, setLike] = useState(false);
 
   useEffect(() => {
     axios({
@@ -20,30 +20,12 @@ const Row = ({ title, fetchURL }) => {
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
       <div className="flex relative items-center">
-        <div id={"slider"}>
+        <div
+          id={"slider"}
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
+        >
           {movies.map((item, id) => (
-            <div
-              key={id}
-              className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2"
-            >
-              <img
-                className="w-full h-auto block"
-                src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
-                alt={item?.title}
-              />
-              <div className="absolute top-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white duration-200">
-                <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
-                  {item?.title}
-                </p>
-                <p>
-                  {like ? (
-                    <FaHeart className="absolute top-4 left-4 text-gray-300 " />
-                  ) : (
-                    <FaRegHeart />
-                  )}
-                </p>
-              </div>
-            </div>
+            <Movie key={id} item={item} />
           ))}
         </div>
       </div>
