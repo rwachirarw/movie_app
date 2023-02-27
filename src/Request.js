1ebed0acaef9509dd3e244063bd45af8b7065a36
Edwin Njeru <rwachira.rw@gmail.com>
2023-02-27 13:41:29 +0300

    first commit

---
+++ src/Request.js
@@ -0,0 +1,11 @@
+const key = "8f44aa0aa0b58b425620a10abaaf9e29";
+
+const requests = {
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+  requestTrending: `https://api.themoviedb.org/3/trending/all/day?api_key=${key}`,
+  requestHorror: ``,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+};
+
+export default requests;
